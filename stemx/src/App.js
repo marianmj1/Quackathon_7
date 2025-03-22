f6e7686f2b5066d30ab868c10b99925fe0ecddc2
marianmj1 <moralesjimenez@oakland.edu>
2025-03-22 15:49:15 -0400

    frontend work

--- stemx/src/App.js
+++ stemx/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage.js';
+// Import other pages like About, Login, Signup
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* Add these as you build them */}
+        {/* <Route path="/about" element={<AboutPage />} /> */}
+        {/* <Route path="/login" element={<LoginPage />} /> */}
+        {/* <Route path="/signup" element={<SignupPage />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
